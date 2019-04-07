c6b68b51ace397a698554684e808247be2509b85
Thisseas X <thisseasx@yahoo.com>
2019-04-07 22:55:27 +0300

    Lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ang-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   });
 });
